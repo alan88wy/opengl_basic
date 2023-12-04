bd0051579a78fb645fd16f0f95d372b1cc6855e8
alan88wy <10415010+alan88wy@users.noreply.github.com>
2023-12-04 21:34:44 +0800

    Added rotation and translation of triangle

--- main_v2.cpp
+++ main_v2.cpp
@@ -1,32 +1,19 @@
-#define _USE_MATH_DEFINES
-
 #include <stdio.h>
 #include <string.h>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <stdlib.h>
 #include <cmath>
-//#include <GLM/vec3.hpp> // glm::vec3
-//#include <GLM/vec4.hpp> // glm::vec4
-//#include <GLM/mat4x4.hpp> // glm::mat4
-#include <GLM/glm.hpp>  // vec3, vec4, mat4 etc
-#include <GLM/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
-#include <GLM/gtc/type_ptr.hpp>
-
-
 
 // Window Dimentions
 const GLint WIDTH = 800, HEIGHT = 600;
-const float toRadians = M_PI / 180.0f; // convert to radian 3.14159265f
 
-GLuint VAO, VBO, shader, uniformModel;
+GLuint VAO, VBO, shader, uniformXMove;
 
 bool direction = true;
 float triOffset = 0.0f;
 float triMaxOffset = 0.7f;
-float triIncrement = 0.0007f;
-
-float curAngle = 0.0f;
+float triIncrement = 0.0005f;
 
 // Create Shader. Normally, you create in external file
 
@@ -36,11 +23,11 @@ static const char* vShader = "                                               \n\
                                                                              \n\
 layout (location = 0) in vec3 pos;                                           \n\
                                                                              \n\
-uniform mat4 model;                                                          \n\
+uniform float xMove;                                                         \n\
                                                                              \n\
 void main()                                                                  \n\
 {                                                                            \n\
-     gl_Position = model * vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);       \n\
+     gl_Position = vec4(0.6 * pos.x + xMove, 0.6 * pos.y, pos.z, 1.0);       \n\
 }";
 
 
@@ -64,30 +51,20 @@ void CreateTriangle()
         0.0f, 1.0f, 0.0f
     };
 
-    // VAO - Vertex array object
-    //       A vertex array object (also known as VAO) can be bound just like a vertex buffer 
-    //       object and any subsequent 
-    //       vertex attribute calls from that point on will be stored inside the VAO.
-    // VBO - Vertex Buffer Object (VBO) to store vertex data in GPU memory
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
 
-    glGenVertexArrays(1, &VAO);  // glGenVertexArrays(GLsizei n, GLuint* arrays); generate vertex array object names
-    glBindVertexArray(VAO);      // bind a vertex array object
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
-    glGenBuffers(1, &VBO);       // glGenBuffers(GLsizei n, GLuint* buffers); generate buffer object names
-    glBindBuffer(GL_ARRAY_BUFFER, VBO); // glBindBuffer(GLenum target, GLuint buffer); bind a named buffer object
-
-    // glBufferData, glNamedBufferData — creates and initializes a buffer object's data store 
-    // void glBufferData(GLenum target, GLsizeiptr size, const void* data, GLenum usage);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    // void glVertexAttribPointer(GLuint index, GLint size, GLenum type, GLboolean normalized, GLsizei stride,
-    //                            const void* pointer);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0); // define an array of generic vertex attribute data
-    glEnableVertexAttribArray(0); // void glEnableVertexAttribArray(GLuint index); Enable or disable generic vertex attribute array
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
 
-    glBindBuffer(GL_ARRAY_BUFFER, 0); // Unbind the buffer because we set it to 0
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-    glBindVertexArray(0);  // Remove after use. Unbind the buffer because we set it to 0
+    glBindVertexArray(0);  // Remove after use
 }
 
 void AddShader(GLuint theProgram, const char* shaderCode, GLenum shaderType) 
@@ -119,7 +96,6 @@ void AddShader(GLuint theProgram, const char* shaderCode, GLenum shaderType)
 
 
     glAttachShader(theProgram, theShader);
-
     return;
 }
 
@@ -157,7 +133,7 @@ void CompileShaders()
         return;
     }
 
-    uniformModel = glGetUniformLocation(shader, "model");
+    uniformXMove = glGetUniformLocation(shader, "xMove");
 
 }
 
@@ -240,35 +216,13 @@ int main()
             direction = !direction;
         }
 
-        curAngle += 0.005f;
-
-        // Not necessay but good to do to ensure it is not to large after running for a while
-        if (curAngle >= 360) {
-            curAngle -= 360;
-        }
-
         // clear window
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);  // no divide by 256. RGB Color (r, g, b) plus transparancy
         glClear(GL_COLOR_BUFFER_BIT);  // Clear all colors;
 
         glUseProgram(shader);
 
-        // glm::mat4 model(1.0f);
-        // Or alternatively :
-        // glm::mat4 model = glm::mat4(1.0f);
-
-        glm::mat4 model = glm::mat4(1.0f);
-        
-        // order is important
-        model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));  // We only translate x value with triOffset
-        model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 0.0f, 1.0f)); // Rotate along y axis
-
-        // void glUniform1f(GLint location, GLfloat v0); v0 = triOffset
-        glUniform1f(uniformModel, triOffset);  // Specify the value of a uniform variable for the current program object
-
-        // Specify the value of a uniform variable for the current program object
-        // void glUniformMatrix4fv(GLint location, GLsizei count, GLboolean transpose, const GLfloat * value);
-        glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+        glUniform1f(uniformXMove, triOffset);
 
         glBindVertexArray(VAO);
 
