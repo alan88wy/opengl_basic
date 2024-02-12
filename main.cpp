4825c45e195ab1b9c51361f780374747c4820df9
alan88wy <10415010+alan88wy@users.noreply.github.com>
2024-02-12 22:53:26 +0800

    Added comment

--- main.cpp
+++ main.cpp
@@ -34,6 +34,7 @@
 // Radian is a unit of measurement of an angle.
 // pi = 3.14159265 = 180 degree
 // 360 degree = 2*pi radian
+// 
 //
 // 1 degree = (2pi Radians)/360° = pi / 180;
 // const float toRadians = 3.14159265f / 180.0f;
